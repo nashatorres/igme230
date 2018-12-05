9049cb6ee20fafa71a13ac0a6b1b6acbc661c228
Nasharilys Torres <nasharilystorres@nasharilyss-mbp.wireless.rit.edu>
2018-12-04 19:25:24 -0500

    updatedpractical

--- fall2018sample/fall2018sample.js
+++ fall2018sample/fall2018sample.js
@@ -7,12 +7,8 @@ $(".menubox").click(function () {
 let content = ("content1.txt") // sets default verse element
 $("#choose-content").val(content); // changes menu option to default
 $("#content").load(content); // retrieves only default element
-$("#choose-content").change(function () {
+$("#choose-content").change(function () { // can put click to replace change in order to have user click for event to happen
     verse = $(this).val();
     $("#content").load(verse);
 });
 
-$("#choose-content").change(function(){
-    selection = $(this).val();
-    $("#content").load(select);
-});
